11a7579475cdae327fe2b359ae39c57f93fc9480
Rajan <Rajanroop786@gmail.com>
2019-11-12 00:47:20 +1300

    Add project files.

---
+++ Factory.cs
@@ -0,0 +1,25 @@
+﻿namespace RocketRace
+{
+    public static class Factory
+    {
+        public static int GuyNumber = 0;
+        public static Punter GetNameId(string name)
+        {
+            switch (name)
+            {
+                case "Jonny":
+                    GuyNumber = 0;
+                    return new Jonny();
+
+                case "Anil":
+                    GuyNumber = 1;
+                    return new Anil();
+
+                case "Al":
+                    GuyNumber = 2;
+                    return new Al();
+            }
+            return new Jonny();
+        }
+    }
+}
---
+++ Jonny.cs
@@ -0,0 +1,12 @@
+﻿namespace RocketRace
+{
+    public class Jonny : Punter
+
+    {
+        public Jonny()
+        {
+            Name = "Jonny";
+            Cash = 50; // he has got 50$
+        }
+    }
+}
